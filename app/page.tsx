8da0425796dbe4bf3a6e26b89cfbea0e26bfe189
pandanoir <moon_in_france@yahoo.co.jp>
2022-12-30 17:17:52 +0900

    simplify code

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,13 @@
-'use client';
-import Link from 'next/link';
-import { useEffect } from 'react';
-import { Scrambow } from 'scrambow';
+"use client";
 
-const scrambler = new Scrambow();
-const TopPage = () => {
-  useEffect(() => {
-    console.log();
-    console.log(scrambler.setSeed(1).get());
-  });
-  return (
-    <div>
-      <main>
-        <h1>Top page</h1>
-        <Link href="/todo">app page</Link>
-        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-        <a href="/api/auth/login">login</a>
-      </main>
-    </div>
-  );
-};
+var str;
+str = "";
+for (let i = 0; i < 3; i++) {
+  if (true) {
+    (str += [`${1 + i}`, "a"][0]), (str += " ");
+  }
+}
+console.log(str);
+
+const TopPage = () => null;
 export default TopPage;
